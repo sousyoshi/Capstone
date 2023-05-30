474dbcfd02c754ff1770456e443e55f4771d4019
Joshua <joshdante@hotmail.com>
2023-05-30 11:11:55 -0400

    validations

--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -31,7 +31,7 @@ function SignupFormModal() {
 
 	return (
 		<>
-			
+
 			<form className="signupContainer" onSubmit={handleSubmit}>
 				<ul>
 					{errors.map((error, idx) => (
@@ -74,7 +74,7 @@ function SignupFormModal() {
 						required
 					/>
 				</label>
-				<button type="submit">Sign Up</button>
+				<button type="submit" disabled={password.length < 6 || !confirmPassword.length || !email.length || !username.length}>Sign Up</button>
 			</form>
 		</>
 	);
